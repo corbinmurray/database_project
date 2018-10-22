0997535badb3de6bcf3a967f330f90061c63183b
Corbin <corbin.murray63@yahoo.com>
2018-10-22 12:26:07 -0500

    test

---
+++ src/data.py
@@ -0,0 +1,19 @@
+import requests
+
+
+
+class Data:
+
+    def __init__(self, city_name, city_country):
+        self.API_KEY = "7e78e9dc22987c9b04c69f1dd31bc505"
+        self.BASE_URL = "http://api.openweathermap.org/data/2.5/weather"
+
+        
+        
+
+
+
+# Wichita = Data(city_name="Wichita", city_country="United States")
+
+
+
