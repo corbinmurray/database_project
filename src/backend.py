7172cec46c071420844652e33c944e4263a293f2
Corbin <corbin.murray63@yahoo.com>
2018-10-24 13:41:01 -0500

    Makin' that progress :)

--- src/backend.py
+++ src/backend.py
@@ -1,5 +1,7 @@
 import sqlite3 
 from data import Data
+from datetime import datetime
+from sqlite3 import Error
 
 
 class Database:
@@ -9,14 +11,43 @@ class Database:
 
     def create_table(self):
 
-        sql_cmmnd = "CREATE TABLE IF NOT EXISTS Weather(city TEXT, date TEXT, coord BLOB, main TEXT, visibility INTEGER, conditions TEXT, wind TEXT)"
+        sql_commnd = "CREATE TABLE IF NOT EXISTS Weather\
+                        (city TEXT, date TEXT, coord TEXT, weather TEXT, visibility INTEGER, temp INTEGER, tempMin INTEGER, tempMax INTEGER, pressure INTEGER, humidity INTEGER, wind TEXT)"
 
         try:
-            self.curs.execute(sql_cmmnd)
+            self.curs.execute(sql_commnd)
         except Error as e:
             print(e)
             print("Error executing create_table")
- 
+
+    def insert(self, data_obj):
+        # Anything CAPITALIZED is a sql command 
+        today_date = datetime.today()
+        today = "{month}-{day}-{year}".format(month=today_date.month,day=today_date.day, year=today_date.year)
+        
+        sql_insert_command = "INSERT INTO Weather (city, date, coord, weather, visibility, temp, tempMin, tempMax, pressure, humidity, wind) VALUES (?,?,?,?,?,?,?,?,?,?,?)"
+        
+        value_tuple = [] 
+
+        value_tuple.append(data_obj.weather_data["name"])
+        value_tuple.append(today)
+        value_tuple.append(str(data_obj.weather_data["coord"]))
+        value_tuple.append(str(data_obj.weather_data["weather"][0]))
+        value_tuple.append(data_obj.weather_data["visibility"])
+        value_tuple.append(data_obj.weather_data["main"]["temp"])
+        value_tuple.append(data_obj.weather_data["main"]["temp_min"])
+        value_tuple.append(data_obj.weather_data["main"]["temp_max"])
+        value_tuple.append(data_obj.weather_data["main"]["pressure"])
+        value_tuple.append(data_obj.weather_data["main"]["humidity"])
+        value_tuple.append(str(data_obj.weather_data["wind"]))
+
+        value_tuple = tuple(value_tuple)
+
+        try:
+            self.curs.execute(sql_insert_command, value_tuple)
+            self.conn.commit()
+        except Error as e:
+            print(e, "\nError inserting")
 
         
     def open(self):
@@ -31,6 +62,22 @@ class Database:
 
         self.conn.close()
 
+    def view_all(self):
+
+        self.curs.execute("SELECT * FROM Weather")
+        
+
+        [print(row, "\n") for row in self.curs.fetchall()]
+    
+    def delete_table(self):
+
+        self.curs.execute("DROP TABLE IF EXISTS Weather")
+        self.conn.commit()
+
+
+
+      
+
 
 
 
@@ -41,6 +88,5 @@ test = Database("weather.db")
 
 test.open()
 
-test.create_table()
 
 test.close()
