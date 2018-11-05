c664b99fb58cc931bb2c6592fcf222d19cbf1f57
Corbin <corbin.murray63@yahoo.com>
2018-11-05 13:37:31 -0600

    anything you want

--- src/backend.py
+++ src/backend.py
@@ -159,7 +159,6 @@ class Database:
 
         self.close()
 
-<<<<<<< HEAD
     def get_weather(self, city_name, user_date):
         self.open()
 
@@ -173,83 +172,3 @@ class Database:
             print("Error in get_weather")
 
         self.close()
-=======
-class Data:
-
-    def __init__(self, city_name, city_country):
-
-        self.API_KEY = "7e78e9dc22987c9b04c69f1dd31bc505"
-        self.BASE_URL = "http://api.openweathermap.org/data/2.5/weather"
-        self.city = city_name
-        self.country = city_country
->>>>>>> 025dcf5cc525a5d43bf9e4d952a8bf8e91d7bd6b
-
-        self.weather_data = self.download_json()
-
-        self.scrub()
-        self.change_units()
-
-
-    def download_json(self):
-
-<<<<<<< HEAD
-test = Database("/Users/corbinmurray/Projects/database_env/database_project/data/weather.db")
-
-city_list = ["Wichita", "Chicago", "Denver", "Austin", "Sacramento", "Kalispell", "Billings", "Houston", "Seattle", "Nashville", "Milwaukee", "Atlanta", "Lincoln", "Reno","Wells"]
-
-country = "United States"
-
-for city in city_list:
-    data = Data(city_name=city, city_country=country)
-
-    test.insert(data)
-=======
-        params = self.city + "," + self.country
-        payload = {"q": params, "appid": self.API_KEY, "units": "imperial"}
-        req = requests.get(self.BASE_URL, params = payload)
-
-        return req.json()
-
-    def scrub(self):
-        # base, clouds, cod, dt, id, sys, [weather][icon], [weather][id]
-
-        del self.weather_data["base"]
-        del self.weather_data["clouds"]
-        del self.weather_data["cod"]
-        del self.weather_data["dt"]
-        del self.weather_data["id"]
-        del self.weather_data["sys"]
-        del self.weather_data["weather"][0]["icon"]
-        del self.weather_data["weather"][0]["id"]
-
-    def change_units(self):
-        # 33.8638 hPa to 1 inHg -> converting hPa to inHg
-        self.weather_data["main"]["pressure"] /= 33.8638
-        self.weather_data["main"]["pressure"] = round(self.weather_data["main"]["pressure"], 2)
-
-        # 0.000621371 miles in 1 meter -> meters to miles
-        self.weather_data["visibility"] *= 0.000621371
-        self.weather_data["visibility"] = round(self.weather_data["visibility"], 2) 
-
-        # 0 is N, 90 is E, 180 is S, 270 is W  -> changing meterological directions to cardinal
-        try:
-            degree = self.weather_data["wind"]["deg"]
-            if degree > 0 and degree < 90:
-                self.weather_data["wind"]["deg"] = "NE"
-            elif degree > 90 and degree < 180:
-                self.weather_data["wind"]["deg"] = "SE"
-            elif degree > 180 and degree < 270:
-                self.weather_data["wind"]["deg"] = "SW"
-            elif degree > 270 and degree < 360:
-                self.weather_data["wind"]["deg"] = "NW"
-            elif degree == 0:
-                self.weather_data["wind"]["deg"] = "N"
-            elif degree == 90:
-                self.weather_data["wind"]["deg"] = "E"
-            elif degree == 180:
-                self.weather_data["wind"]["deg"] = "S"
-            elif degree == 270:
-                self.weather_data["wind"]["deg"] = "W"
-        except KeyError:
-            pass
->>>>>>> 025dcf5cc525a5d43bf9e4d952a8bf8e91d7bd6b
--- src/frontend.py
+++ src/frontend.py
@@ -28,11 +28,31 @@ class HomePage(tkinter.Frame):
     def __init__(self, master):
         tkinter.Frame.__init__(self, master=master, background="#42f4e5")
 
-        self.home_label = tkinter.Label(master=self, text="Home Page Label Bitches")
-        self.home_label.grid(row=0, column=0)
+        self.columnconfigure(index = 0, weight = 1)
+        #self.columnconfigure(index = 3, weight = 1)
+        self.columnconfigure(index = 4, weight = 1)
+        
+        #self.rowconfigure(index = 0, weight = 1)
+        self.rowconfigure(index = 3, weight = 1)
+        self.rowconfigure(index = 5, weight = 1)
+
+
+        self.home_label = tkinter.Label(master=self, text="Welcome to Weather Finder", justify = "center", font = ("Tahoma", 18))
+        self.home_label.grid(row=0, column=2, pady = 20)
+
+
+        self.city_label = tkinter.Label(master = self, text = "Pick Your City", font = ("Tahoma", 16), relief = "sunken")
+        self.city_label.grid(row = 2, column = 1)
+
+        self.date_label = tkinter.Label(master = self, text = "Choose A Date", font = ("Tahoma", 16))
+        self.date_label.grid(row = 2, column = 3, pady = 20)
+
+        self.run_button = tkinter.Button(master = self, text = "Run", font = ("Tahoma", 14), relief = "sunken")
+        self.run_button.grid(row = 4, column = 2)
 
 
 
-test = WeatherApp(window_name="Weather Application", window_size="600x500")
+
+test = WeatherApp(window_name="Fiesta Weather", window_size="500x400")
 
 test.run()
